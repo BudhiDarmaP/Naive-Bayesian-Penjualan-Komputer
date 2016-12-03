28afdc4262671e18e261f741f2f0307904153482
budhidarmap <budhidarmap@DESKTOP-8U7BB6O>
2016-12-03 15:24:21 +0700

    12/2/2016 Tanpa GUI

--- test/test.java
+++ test/test.java
@@ -4,12 +4,6 @@ import Model.NaiveBayes;
 import Model.Probabilitas;
 import java.util.Arrays;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author budhidarmap
